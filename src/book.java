0a432959cbe4e4093c400e19abe2dfb53c7a9cfa
Your Name <92316310+DosBalzhan@users.noreply.github.com>
2021-11-24 19:33:20 +0600

    commit

---
+++ src/book.java
@@ -0,0 +1,11 @@
+public class book implements izdanie{
+    @Override
+    public void zhanr(){
+        System.out.println("Scazka");
+
+    }
+    @Override
+    public void year(){
+        System.out.println("2007");
+    }
+}
---
+++ src/enciklopedya.java
@@ -0,0 +1,11 @@
+public class enciklopedya implements izdanie{
+    @Override
+    public void zhanr(){
+        System.out.println("Science");
+
+    }
+    @Override
+    public void year(){
+        System.out.println("2010");
+    }
+}
